2a793f291b5d2a4bff78bb8f18123b010fb64567
praneethnamburi <praneeth.namburi@gmail.com>
2019-01-11 19:32:32 -0500

    blender hello world accomplished

--- helloBlender.py
+++ helloBlender.py
@@ -1,1 +1,9 @@
-# this is a comment
+import bpy
+from random import randint
+
+n = 500
+for i in range(0,n):
+    x = randint(-30, 30)
+    y = randint(-30, 30)
+    z = randint(-30, 30)
+    bpy.ops.mesh.primitive_monkey_add(location=(x,y,z))
