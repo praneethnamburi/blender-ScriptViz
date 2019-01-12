301f5b3487cae10899a69c68bdf8b5a2f8cc6fdf
praneethnamburi <praneeth.namburi@gmail.com>
2019-01-12 14:36:51 -0500

    suppressed pylint warning

--- helloBlender.py
+++ helloBlender.py
@@ -1,9 +1,9 @@
-import bpy
+import bpy #pylint: disable=import-error
 from random import randint
 
-n = 500
+n = 5
 for i in range(0,n):
-    x = randint(-30, 30)
-    y = randint(-30, 30)
-    z = randint(-30, 30)
-    bpy.ops.mesh.primitive_monkey_add(location=(x,y,z))
+    x = randint(-5, 5)
+    y = randint(-5, 5)
+    z = randint(-5, 5)
+    bpy.ops.mesh.primitive_monkey_add(location=(x,y,z))    
