900f9675735f0a575bba5324ce97c813d1e10cf1
praneethnamburi <praneeth.namburi@gmail.com>
2019-01-26 04:26:21 -0500

    removed dated blender config files

--- blender config/blenderStart.py
+++
@@ -1,9 +0,0 @@
-"""
-Run this code when you start blender.
-I am yet to find a way to automate this.
-"""
-
-import sys
-sys.path.append('C:\\Workspace\\blenderPython')
-import bpnModule
-bpn = bpnModule.bpnClass(bpy) # bpn is an object
