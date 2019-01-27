3c4d73f67dd9b5e71e3218335b988b5aa97b5e59
Praneeth <praneeth.namburi@gmail.com>
2019-01-26 19:17:15 -0500

    generate package requirements on every blender launch

--- _launch.py
+++ _launch.py
@@ -2,5 +2,7 @@
 ### Then use exec(bpy.loadStr) in the blender python console
 import bpy #pylint: disable=import-error
 import os
+
 launchFileName = os.path.join(os.path.dirname(os.path.realpath(__file__)), '_launchContents.py')
 bpy.loadStr = open(launchFileName).read()
+os.system('pip freeze > requirements.txt')
