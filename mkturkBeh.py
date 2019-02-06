f9aa82dd6306cff3f531304446606ffddc61218e
praneethnamburi <praneeth.namburi@gmail.com>
2019-02-06 05:13:18 -0500

    set up behavior query from python

--- mkturkBeh.py
+++ mkturkBeh.py
@@ -1,7 +1,14 @@
+"""
+Analyze mkturk behavior data.
+"""
 import json
 import subprocess
 
-subprocess.call(['node', 'testjs.js'])
-tmp1 = json.loads(open('temp.json').read())
+agent = 'Sausage'
+startDate = '20190201'
+endDate = '20190205'
+outFile = f'./_temp/{agent}{startDate}to{endDate}.json'
 
-a = 1
+subprocess.run(f'node mkturkBeh_getData.js -a {agent} -s {startDate} -e {endDate} -o {outFile} --no-print')
+tmp1 = json.loads(open(outFile).read())
+print(len(tmp1.keys()))
