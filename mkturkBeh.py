1637828819417ae5544dfc9bb442c1df0591cb65
praneethnamburi <praneeth.namburi@gmail.com>
2019-02-08 04:23:30 -0500

    mkturk behavior response time histograms

--- mkturkBeh.py
+++ mkturkBeh.py
@@ -30,7 +30,12 @@ class behSession:
         files into one json object, then you can make a list of behSession
         objects using:
             beh = [behSession(file, behAll[file]) for file in behAll]
+        Note that time is in ms
+        
+    List of variables and their description:
+        https://github.com/dicarlolab/mkturk/tree/master/public
     TODO: If only the file name is given, then get it from the database directly
+    TODO: Tag trials - response
     """
     # pylint: disable=no-member
     def __init__(self, fName, b):
@@ -42,28 +47,90 @@ class behSession:
                 setattr(self, attr, np.array([np.array(k) for k in b[attr].values()]).T) # 2d numpy arrays
             else:
                 setattr(self, attr, b[attr])
+        self._timeUnits = 'ms'
+        self._tmul = 1.0
+
         self.nTrials = np.size(self.Response)
+        self.fixExtraTrial()
+        self.refTime = self.StartTime[0] # use this as start of the task?        
+ 
+    def properties(self):
+        """Print all the properties in this class."""
+        [print((k, type(getattr(self, k)), np.shape(getattr(self, k)))) for k in dir(self) if '_' not in k and 'method' not in k]
+
+    def fixExtraTrial(self):
+        """
+        mkturk adds an extra entry to variables set up before the trial is finished.
+        This function discards that extra entry.
+        """
+        flds = ['CorrectItem', 'FixationGridIndex', 'Sample', 'StartTime', 'Test']
+        for fld in flds:
+            if self.nTrials+1 in np.shape(getattr(self, fld)):
+                setattr(self, fld, getattr(self, fld)[:-1])
+       
+    @property
+    def timeUnits(self):
+        """Units to be used when dealing with time. (s, ms)"""
+        return self._timeUnits
+
+    @timeUnits.setter
+    def timeUnits(self, thisUnits):
+        """Set the time units. Default is ms."""
+        assert thisUnits in ['s', 'ms']
+        self._timeUnits = thisUnits
+        if thisUnits == 'ms':
+            self._tmul = 1.0
+        elif thisUnits == 's':
+            self._tmul = 1/1000
+        elif thisUnits == 'min':
+            self._tmul = 1/(1000*60)
+        elif thisUnits == 'hr':
+            self._tmul = 1/(1000*60*60)
+
+    @property
+    def validTrials(self):
+        """
+        Boolean of shape (nTrials,).
+        False if the trial timed out.
+        """
+        return self.Response != -1
 
     @property
     def accuracy(self):
-        """Accuracy for that session, across all trials."""
-        return np.mean(self.Response == self.CorrectItem)
+        """Accuracy within session across valid trials."""
+        return np.mean(self.Response[self.validTrials] == self.CorrectItem[self.validTrials])
+
+    @property
+    def rts(self):
+        """Response times (includes invalid trials)."""
+        return self.ResponseXYT[:, -1] - self.FixationXYT[:, -1]
+    
+    @property
+    def rts_valid(self):
+        """Returns response times (only valid trials)."""
+        return self.rts[self.validTrials]
+
+    @property
+    def rt(self):
+        """Mean Response time across valid trials."""
+        return np.mean(self.rts[self.validTrials])
+
 
 beh = [behSession(file, behAll[file]) for file in behAll]
 
 #%%
-type(beh[0])
-
-#%%
 import matplotlib.pyplot as plt
-import numpy as np
 
-α = np.linspace(0, 20, 100)
 h = plt.figure(figsize=(16, 10))
-nRows = 2
+nRows = 4
 nCols = 2
-allAx = h.subplots(nRows, nCols)
-for imCount in range(nRows*nCols):
-    rowCount = imCount // nCols
-    colCount = imCount % nCols
-    hIm = allAx[rowCount][colCount].plot(α, np.sin(α))
+allAx = h.subplots(nRows, nCols, sharey=False, sharex=True)
+for sessionCount in range(np.size(beh)):
+    rowCount = sessionCount // nCols
+    colCount = sessionCount % nCols
+    #hIm = allAx[rowCount][colCount].plot(np.sort(beh[sessionCount].rts_valid))
+    hIm = allAx[rowCount][colCount].hist(beh[sessionCount].rts_valid, bins=np.linspace(0, 2000, 40), density=True)
+    allAx[rowCount][colCount].set_title(beh[sessionCount].fName)
+    
+plt.xlim(0, 2000)
+h.suptitle('Response times')
