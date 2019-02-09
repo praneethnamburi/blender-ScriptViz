324c5086b5409669511b822910d813d1e6bb40a2
praneethnamburi <praneeth.namburi@gmail.com>
2019-02-09 03:45:24 -0500

    minor

--- mkturkBeh.py
+++ mkturkBeh.py
@@ -1,6 +1,7 @@
 """
 Analyze mkturk behavior data.
 """
+#pylint: disable=fixme
 
 #%%
 import json
@@ -73,6 +74,7 @@ class behSession:
  
     def properties(self):
         """Print all the properties in this class."""
+        #pylint:disable=expression-not-assigned
         [print((k, type(getattr(self, k)), np.shape(getattr(self, k)))) for k in dir(self) if '_' not in k and 'method' not in k]
 
     def fixExtraTrial(self):
