e5a07722ebfbe873cec1304eb704a866f4523b24
praneethnamburi <praneeth.namburi@gmail.com>
2020-03-08 23:56:29 -0400

    updated defaults for matrix visualization

--- apps/animation/meshViz.py
+++ apps/animation/meshViz.py
@@ -26,15 +26,15 @@ def mat2mesh(z, x=[], y=[]):
     z is a 2-D numpy array, 2D list or an anonymous function of 2 input numbers and one output number
     """
     if isinstance(z, types.FunctionType):
-        if not x: # default ranges
+        if x is mat2mesh.__defaults__[0]: # default ranges
             x = np.arange(-2, 2, 0.1)
-        if not y:
+        if y is mat2mesh.__defaults__[1]:
             y = np.arange(-2, 2, 0.1)
         z = np.array([[z(xv, yv) for yv in y] for xv in x])
     else: # expecting a numpy array or a 2d list
-        if not x:
+        if x is mat2mesh.__defaults__[0]:
             x = np.arange(0, np.shape(z)[0])
-        if not y:
+        if y is mat2mesh.__defaults__[1]:
             y = np.arange(0, np.shape(z)[1])
 
     nX = len(x)
@@ -47,14 +47,14 @@ def mat2mesh(z, x=[], y=[]):
     f = [(iy*nX+ix, iy*nX+ix+1, (iy+1)*nX+(ix+1), (iy+1)*nX+ix) for iy in np.arange(0, nY-1) for ix in np.arange(0, nX-1)]
     return v, f
 
-xyfun = lambda x, y: np.sin(2*np.pi*2*x)
-v1, f1 = mat2mesh(xyfun, list(np.arange(-2, 2, 0.02)), list(np.arange(-2, 2)))
+xyfun = lambda x, y: x*x+y*y
+v1, f1 = mat2mesh(xyfun, x=np.arange(-2, 2, 0.02), y=np.arange(-2, 3))
 
 #create mesh and object
 mesh = bpy.data.meshes.new("wave")
 obj = bpy.data.objects.new("wave", mesh)
 bpy.data.collections['Collection'].objects.link(obj)
- 
+
 #create mesh from python data
 mesh.from_pydata(v1, [], f1)
 mesh.update(calc_edges=True)
