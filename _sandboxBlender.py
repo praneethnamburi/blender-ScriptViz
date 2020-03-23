b824a411d55125f974d4fd56072a9cb48ebeff95
praneethnamburi <praneeth.namburi@gmail.com>
2020-03-23 03:00:51 -0400

    Tube cross-section in sandbox: working

--- _sandboxBlender.py
+++ _sandboxBlender.py
@@ -33,6 +33,9 @@ reload(bpn)
 bpn.env.reset()
 
 class Tube(bpn.Msh):
+    """
+    Creates a 'Tube' object from a 3d plot.
+    """
     def __init__(self, name=None, x=0, y=0, z=0, **kwargs):
         names, kwargs = bpn.utils.clean_names(name, kwargs, {'msh_name':'tube_msh', 'obj_name':'tube_obj', 'priority_obj':'new', 'priority_msh':'new'})
         kwargs_ngon, kwargs = pn.clean_kwargs(kwargs, {'n':6, 'r':0.3, 'theta_offset_deg':-1}, {'n':['segments', 'seg', 'u', 'n'], 'r':['radius', 'r'], 'theta_offset_deg':['theta_offset_deg', 'th', 'offset', 'th_off_deg']})
@@ -50,7 +53,48 @@ class Tube(bpn.Msh):
         if kwargs['subsurf']:
             self.subsurf(kwargs_this['subsurf_levels'], kwargs_this['subsurf_render_levels'])
 
-        self.xsec = [bpn.turtle.DirectedSubMsh(self, normals[i, :], **s) for i, s in enumerate(a_exp)]
+        self.xsec = self.XSec(self, normals, a_exp)
+
+    class XSec:
+        """Cross sections of a tube: a collection of DirectedSubMsh's"""
+        def __init__(self, parent, normals, draw_export):
+            self.all = [bpn.turtle.DirectedSubMsh(parent, normals[i, :], **s) for i, s in enumerate(draw_export)]
+            self._normals = normals
+
+        @property
+        def n(self):
+            """Number of cross sections."""
+            return len(self.all)
+
+        @property
+        def centers(self):
+            """The 'spine' of the tube. nCrossSections X 3 numpy array."""
+            return np.array([x.center for x in self.all])
+        
+        @centers.setter
+        def centers(self, new_centers):
+            new_centers = np.array(new_centers)
+            assert np.shape(new_centers) == (self.n, 3)
+            for i in range(self.n):
+                self.all[i].center = new_centers[i, :]
+
+        def update_normals(self):
+            """Update normals based on the location of the centers."""
+            spine = self.centers
+            self.normals = np.vstack((spine[1, :] - spine[0, :], spine[2:, :] - spine[:-2, :], spine[-1, :] - spine[-2, :]))
+
+        @property
+        def normals(self):
+            """Normals of each x-section"""
+            return np.array(self._normals)
+
+        @normals.setter
+        def normals(self, new_normals):
+            """Set """
+            new_normals = np.array(new_normals)
+            assert np.shape(new_normals) == (self.n, 3)
+            for i in range(np.shape(new_normals)[0]):
+                self.all[i].normal = new_normals[i, :]
 
 θ = np.radians(np.arange(0, 360+40, 40))
 z1 = np.sin(θ)
@@ -58,10 +102,16 @@ y1 = np.cos(θ)
 x1 = θ/2
 
 t = Tube('myTube', x=x1, y=y1, z=z1, n=4, th=0, shade='smooth', subsurf=True)
-t.xsec[0].normal = (0, 0, 1)
-t.xsec[-1].scale(3)
-t.xsec[-1].normal = (0, 0, 1)
+t.xsec.all[-1].scale(3)
+t.xsec.all[-1].normal = (0, 0, 1)
+
+t.xsec.centers = np.vstack((z1, y1, x1)).T
+t.xsec.update_normals()
+
 t.morph(frame_start=100)
+bpn.env.Key().goto(150)
+
+# print(t.xsec.centers)
 
 # b[0].center = (0, 1, 0)
 
