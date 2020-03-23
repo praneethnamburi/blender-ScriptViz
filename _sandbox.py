e2db0e481ffdef9396bbe096584228d40a257c40
praneethnamburi <praneeth.namburi@gmail.com>
2020-03-23 10:50:34 -0400

    minor: doc

--- _sandbox.py
+++ _sandbox.py
@@ -1,10 +1,16 @@
 """
 Sandbox. Not involving blender.
+
+Mode 1:
 Type code here, and press Ctrl+Enter (python.datascience.runFileInteractive)
 This opens an ipython session and evaluates the code there.
 In the current setup, I'm initiating the kernel from blender's python.
-CAUTION: Use this only when debugger for blender is not active!
+
+Mode 2:
+To debug this code, simply insert breakpoint(s) and press f5 (make sure you don't have cells - #%%)
+
+CAUTION: Use these modes only when debugger for blender is not active!
 """
-#%%
+
 import os
 os.chdir('D:\\Workspace\\blenderPython')
