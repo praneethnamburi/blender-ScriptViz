64b962306af581b5d7da152135c3947c55ca306d
praneethnamburi <praneeth.namburi@gmail.com>
2020-03-28 20:23:06 -0400

    refactor: moved tube to new

--- bpn/_sandbox.py
+++ bpn/_sandbox.py
@@ -8,76 +8,6 @@ pn.reload()
 env.reset()
 #-----------------
 
-class Tube(core.Msh):
-    """
-    Creates a 'Tube' object with a specified number of cross sections
-    and vertical sections.
-    """
-    def __init__(self, name=None, x=0, y=0, z=0, **kwargs):
-        names, kwargs = bpn.utils.clean_names(name, kwargs, {'msh_name':'tube_msh', 'obj_name':'tube_obj', 'priority_obj':'new', 'priority_msh':'new'})
-        kwargs_ngon, kwargs = pn.clean_kwargs(kwargs, {'n':6, 'r':0.3, 'theta_offset_deg':-1}, {'n':['segments', 'seg', 'u', 'n'], 'r':['radius', 'r'], 'theta_offset_deg':['theta_offset_deg', 'th', 'offset', 'th_off_deg']})
-        kwargs_this, kwargs_bpnmsh = pn.clean_kwargs(kwargs, {'shade':'smooth', 'subsurf':True, 'subsurf_levels':2, 'subsurf_render_levels':2})
-        
-        spine = np.array([np.array((tx, ty, tz)) for tx, ty, tz in zip(x, y, z)])
-        normals = np.vstack((spine[1, :] - spine[0, :], spine[2:, :] - spine[:-2, :], spine[-1, :] - spine[-2, :]))
-
-        a = Draw(**names)
-        a.skin(spine, **kwargs_ngon)
-        a_exp = a.export()
-        -a
-        super().__init__(**{**names, **kwargs_bpnmsh})
-        self.shade(kwargs_this['shade'])
-        if kwargs_this['subsurf']:
-            self.subsurf(kwargs_this['subsurf_levels'], kwargs_this['subsurf_render_levels'])
-
-        self.xsec = self.XSec(self, normals, a_exp)
-
-    class XSec:
-        """Cross sections of a tube: a collection of DirectedSubMsh's"""
-        def __init__(self, parent, normals, draw_export):
-            self.all = [bpn.turtle.DirectedSubMsh(parent, normals[i, :], **s) for i, s in enumerate(draw_export)]
-            self._normals = normals
-
-        @property
-        def n(self):
-            """Number of cross sections."""
-            return len(self.all)
-
-        @property
-        def centers(self):
-            """The 'spine' of the tube. nCrossSections X 3 numpy array."""
-            return np.array([x.origin for x in self.all])
-        
-        @centers.setter
-        def centers(self, new_centers):
-            new_centers = np.array(new_centers)
-            assert np.shape(new_centers) == (self.n, 3)
-            for i in range(self.n):
-                self.all[i].origin = trf.PointCloud(new_centers[i, :], np.eye(4))
-
-        # def update_normals(self):
-        #     """Update normals based on the location of the centers."""
-        #     spine = self.centers
-        #     self.normals = np.vstack((spine[1, :] - spine[0, :], spine[2:, :] - spine[:-2, :], spine[-1, :] - spine[-2, :]))
-
-        @property
-        def normals(self):
-            """
-            Normals of each x-section.
-            Returns direction in world
-            """
-            return np.array([self.all[i].normal for i in range(self.n)])
-
-        @normals.setter
-        def normals(self, new_normal_dir):
-            """
-            Directions are origin-agnostic.
-            """
-            new_normal_dir = np.array(new_normal_dir)
-            assert np.shape(new_normal_dir) == (self.n, 3)
-            for i in range(np.shape(new_normal_dir)[0]):
-                self.all[i].normal = trf.PointCloud(new_normal_dir[i, :]+self.all[i].origin, np.eye(4))
-
 def test_skin():
     θ = np.radians(np.arange(0, 360*6+40, 40))
     z1 = np.sin(θ)
@@ -96,7 +26,7 @@ def test_tube_01():
     y1 = np.cos(θ)
     x1 = θ/2
 
-    t = Tube('myTube', x=x1, y=y1, z=z1, n=4, th=0, shade='flat', subsurf=True)
+    t = new.Tube('myTube', x=x1, y=y1, z=z1, n=4, th=0, shade='flat', subsurf=True)
 
     x = t.xsec.all[5]
     x.scale((3, 8, 1))
@@ -112,7 +42,7 @@ def test_tube_03_mobius():
     spine = np.array([np.array((tx, ty, tz)) for tx, ty, tz in zip(x1, y1, z1)])
     normals = np.vstack((spine[1, :] - spine[0, :], spine[2:, :] - spine[:-2, :], spine[-1, :] - spine[-2, :]))
 
-    t = Tube('mobius', x=θ, y=np.zeros_like(θ), z=np.zeros_like(θ), n=8, th=0, shade='flat', subsurf=True)
+    t = new.Tube('mobius', x=θ, y=np.zeros_like(θ), z=np.zeros_like(θ), n=8, th=0, shade='flat', subsurf=True)
     t.scale((1, 1, 0.3))
     t.apply_matrix()
 
@@ -143,8 +73,11 @@ def spring():
     z1 = np.sin(θ)
     y1 = np.cos(θ)
     x1 = θ/2
-    s = Tube('spring', x=x1, y=y1, z=z1)
+    s = new.Tube('spring', x=x1, y=y1, z=z1)
     s.xsec.centers = np.vstack((x1/2, y1, z1)).T
     s.morph(frame_start=100)
+    return s
+
+
 
 spring()
--- bpn/new.py
+++ bpn/new.py
@@ -10,7 +10,7 @@ import bpy #pylint: disable=import-error
 import bmesh #pylint: disable=import-error
 import mathutils #pylint: disable=import-error
 
-from . import vef, utils, core, turtle
+from . import vef, utils, core, turtle, trf
 
 def collection(coll_name='Collection'):
     """
@@ -176,3 +176,74 @@ def spiral(n_rot=3, res=10, offset_rot=0, **kwargs):
     z = np.zeros_like(θ)
 
     return core.Msh(x=x, y=y, z=z, **kwargs)
+
+# Enhanced meshes
+class Tube(core.Msh):
+    """
+    Creates a 'Tube' object with a specified number of cross sections
+    and vertical sections.
+    """
+    def __init__(self, name=None, x=0, y=0, z=0, **kwargs):
+        names, kwargs = utils.clean_names(name, kwargs, {'msh_name':'tube_msh', 'obj_name':'tube_obj', 'priority_obj':'new', 'priority_msh':'new'})
+        kwargs_ngon, kwargs = pn.clean_kwargs(kwargs, {'n':6, 'r':0.3, 'theta_offset_deg':-1}, {'n':['segments', 'seg', 'u', 'n'], 'r':['radius', 'r'], 'theta_offset_deg':['theta_offset_deg', 'th', 'offset', 'th_off_deg']})
+        kwargs_this, kwargs_bpnmsh = pn.clean_kwargs(kwargs, {'shade':'smooth', 'subsurf':True, 'subsurf_levels':2, 'subsurf_render_levels':2})
+        
+        spine = np.array([np.array((tx, ty, tz)) for tx, ty, tz in zip(x, y, z)])
+        normals = np.vstack((spine[1, :] - spine[0, :], spine[2:, :] - spine[:-2, :], spine[-1, :] - spine[-2, :]))
+
+        a = turtle.Draw(**names)
+        a.skin(spine, **kwargs_ngon)
+        a_exp = a.export()
+        -a
+        super().__init__(**{**names, **kwargs_bpnmsh})
+        self.shade(kwargs_this['shade'])
+        if kwargs_this['subsurf']:
+            self.subsurf(kwargs_this['subsurf_levels'], kwargs_this['subsurf_render_levels'])
+
+        self.xsec = self.XSec(self, normals, a_exp)
+
+    class XSec:
+        """Cross sections of a tube: a collection of DirectedSubMsh's"""
+        def __init__(self, parent, normals, draw_export):
+            self.all = [turtle.DirectedSubMsh(parent, normals[i, :], **s) for i, s in enumerate(draw_export)]
+            self._normals = normals
+
+        @property
+        def n(self):
+            """Number of cross sections."""
+            return len(self.all)
+
+        @property
+        def centers(self):
+            """The 'spine' of the tube. nCrossSections X 3 numpy array."""
+            return np.array([x.origin for x in self.all])
+        
+        @centers.setter
+        def centers(self, new_centers):
+            new_centers = np.array(new_centers)
+            assert np.shape(new_centers) == (self.n, 3)
+            for i in range(self.n):
+                self.all[i].origin = trf.PointCloud(new_centers[i, :], np.eye(4))
+
+        def update_normals(self):
+            """Update normals based on the location of the centers."""
+            spine = self.centers
+            self.normals = np.vstack((spine[1, :] - spine[0, :], spine[2:, :] - spine[:-2, :], spine[-1, :] - spine[-2, :]))
+
+        @property
+        def normals(self):
+            """
+            Normals of each x-section.
+            Returns direction in world
+            """
+            return np.array([self.all[i].normal for i in range(self.n)])
+
+        @normals.setter
+        def normals(self, new_normal_dir):
+            """
+            Directions are origin-agnostic.
+            """
+            new_normal_dir = np.array(new_normal_dir)
+            assert np.shape(new_normal_dir) == (self.n, 3)
+            for i in range(np.shape(new_normal_dir)[0]):
+                self.all[i].normal = trf.PointCloud(new_normal_dir[i, :]+self.all[i].origin, np.eye(4))
