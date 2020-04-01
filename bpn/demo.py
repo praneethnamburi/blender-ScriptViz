365dcd4423bb572398849ccf08d72a2a0c581c53
praneethnamburi <praneeth.namburi@gmail.com>
2020-04-01 10:50:15 -0400

    refactor: app to demo

--- bpn/demo.py
+++ bpn/demo.py
@@ -10,6 +10,7 @@ Functions:
     spiral  - animates an object along a spiral path
 
     plane_slice - sculpting and morphing
+    tongue - a morph that looks like a tongue
 
 Turtle module:
     draw_basic - Spring-like structure
@@ -242,6 +243,30 @@ def plane_slice():
     len_z = np.max(sph.v[:, -1]) - np.min(sph.v[:, -1])
     sph.translate(z=len_z)
     sph.morph(frame_start=151)
+    return sph
+
+def tongue(poly_smooth=True):
+    """
+    Morph should look like a tongue in one direction, and an alien helmet in another.
+
+    During this, I learned that vertices should always be selected in an interval (it can be with a small epsilon), but exact values don't seem to work.
+    """
+    sph = new.sphere(name='tongue')
+    sph.slice_x().slice_y().slice_z()
+
+    eps = 0.002
+    v = sph.v
+    sel = np.logical_and.reduce((v[:, 0] > -eps, v[:, 0] < eps, v[:, 1] > -eps, v[:, 1] < eps)) #pylint: disable=no-member
+    v[sel, 0:2] = v[sel, 0:2] - 0.2
+    sph.v = v
+    sph.morph(n_frames=25, frame_start=100)
+    env.Key().goto(125)
+    sph.bo.modifiers.new('subd', type='SUBSURF')
+    sph.bo.modifiers['subd'].levels = 2
+    sph.bo.modifiers['subd'].render_levels = 2
+
+    for p in sph.bm.polygons:
+        p.use_smooth = poly_smooth
     return sph
 
 def draw_basic():
