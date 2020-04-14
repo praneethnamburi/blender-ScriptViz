172728d8cf003e28a2fa6509e3113889cedf612d
praneethnamburi <praneeth.namburi@gmail.com>
2020-04-14 08:53:01 -0400

    bezier circle formula

--- _sandbox.py
+++ _sandbox.py
@@ -14,3 +14,14 @@ CAUTION: Use these modes only when debugger for blender is not active!
 
 import numpy as np
 import pandas as pd
+
+import matplotlib.pyplot as plt
+
+a = (np.sqrt(2)/2 - 4*(0.5**3))/(3*(0.5**3))
+t = np.r_[0:1:0.1]
+
+# cubic bezier
+plt.plot((1-t)**3 + 3*t*((1-t)**2) + 3*(a*(t**2))*(1-t), 3*a*((1-t)**2)*t + 3*(1-t)*(t**2) + t**3, 'o')
+# circle
+plt.plot(np.cos(t*np.pi/2), np.sin(t*np.pi/2), 'o')
+plt.axis('square')
