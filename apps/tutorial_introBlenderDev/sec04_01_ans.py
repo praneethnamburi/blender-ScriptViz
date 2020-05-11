f80a2432ffc7024a991a2e06d5a954acd971ddba
praneethnamburi <praneeth.namburi@gmail.com>
2020-05-11 11:17:45 -0400

    cleanup before github upload

--- apps/tutorial_introBlenderDev/sec04_01_ans.py
+++
@@ -1,8 +0,0 @@
-from bpn_init import * # pylint: disable=wildcard-import, unused-wildcard-import
-
-bpy.data.scenes['Scene'].cursor.location[0] = -10
-
-# env.reset()
-
-m = new.cube(name='Cube')
-m.v = m.v*2
