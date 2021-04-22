462db02ecb9c4837ef4d1e3bbaeaa37838039e6c
praneethnamburi <praneeth.namburi@gmail.com>
2021-04-22 01:38:58 -0400

    spawning marginal improvement

--- plots.py
+++ plots.py
@@ -9,26 +9,18 @@ class Spawn:
     def __init__(self, func):
         self.func = func
     def __call__(self, *args, **kwargs):
-        p = mp.Process(target=self.func, args=args, kwargs=kwargs)
-        p.start()
-        return p
-
-class Plot:
-    """
-    h = Plot([2, 3, 10, 3])
-    -h
-    """
-    def __init__(self, arr):
-        self._proc = Spawn(self._plot)(arr)
-
-    @staticmethod
-    def _plot(arr):
-        plt.figure()
-        plt.plot(arr)
-        plt.show()
-
+        self._proc = mp.Process(target=self.func, args=args, kwargs=kwargs)
+        self._proc.start()
+        return self
     def __neg__(self):
         self._proc.terminate()
+
+def _plot(arr):
+    plt.figure()
+    plt.plot(arr)
+    plt.show()
+
+plot = Spawn(_plot)
 
 if __name__ == '__main__':
     # worker needs to be imported, but other than that, this works great from the blender console!
