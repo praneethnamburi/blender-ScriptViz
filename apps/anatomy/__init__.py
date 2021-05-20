7f0cfd40471cc813abab9900bde8d27641feffda
praneeth <praneeth@mit.edu>
2021-05-20 13:59:41 -0400

    added flexibility

--- apps/anatomy/__init__.py
+++ apps/anatomy/__init__.py
@@ -9,18 +9,21 @@ import bpy #pylint: disable=import-error
 import bpn
 
 # ULTIMATE_HUMAN_ANATOMY = "D:\\Dropbox (MIT)\\Anatomy\\Workspace\\Ultimate_Human_Anatomy_Rigged_Blend_2-81\\skeletalSystem_originAtCenter_xForward.blend"
-SKELETON = "D:\\Dropbox (MIT)\\Anatomy\\Workspace\\Ultimate_Human_Anatomy_Rigged_Blend_2-81\\skeletalSystem_originAtCenter_xForward.blend"
+SKELETON = "C:\\Dropbox (MIT)\\Anatomy\\Workspace\\Ultimate_Human_Anatomy_Rigged_Blend_2-81\\skeletalSystem_originAtCenter_xForward.blend"
 
-def load(obj_list=None, coll_name='Bones'):
+def load(obj_list=None, coll_name='Bones', fname=None):
     """
     Load anatomical meshes from the SKELETON file.
     :param obj_list: (list) list of strings specifying the names of bones.
     :param coll_name: (string) target collection to put the bones into.
     """
-    assert os.path.exists(SKELETON)
+    if fname is None:
+        fname = SKELETON
+
+    assert os.path.exists(fname)
     if obj_list is None:
         obj_list = ['Humerus_R', 'Scapula_R', 'Clavicle_R', 'Radius_R', 'Ulna_R']
-    with bpy.data.libraries.load(SKELETON) as (data_from, data_to):
+    with bpy.data.libraries.load(fname) as (data_from, data_to):
         data_to.objects = [name for name in data_from.objects if name in obj_list]
     
     ret = {}
