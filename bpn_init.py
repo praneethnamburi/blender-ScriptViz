e3847860e4dcb2db3a6619ef6b1dafa85121572f
praneeth <praneeth@mit.edu>
2021-05-20 13:30:02 -0400

    pruning

--- bpn_init.py
+++ bpn_init.py
@@ -36,14 +36,18 @@ from bpn.mantle import Pencil, Screen
 from bpn.utils import get
 from pntools import run
 
-# Convenience
-Matrix = mathutils.Matrix
-Vector = mathutils.Vector
+# # Convenience
+# Matrix = mathutils.Matrix
+# Vector = mathutils.Vector
 
 # for using matplotlib from within blender
 import multiprocess
 multiprocess.set_executable(pn.locate_command('python', 'conda', verbose=False).split('\r\n')[0])
 from plots import plot
+
+def reset():
+    env.reset()
+    core.ThingDB = core._ThingDB()
 
 # bridge to julia
 # pylint: disable=no-name-in-module, wrong-import-order
@@ -53,21 +57,21 @@ from plots import plot
 # Pkg.activate() # switch back to the default environment in julia
 # pylint: enable=no-name-in-module, wrong-import-order
 
-# bridge to MATLAB
-try:
-    import matlab.engine
-    ml_eng_installed = True
-except ImportError:
-    print("Unable to detect MATLAB engine.")
-    ml_eng_installed = False
+# # bridge to MATLAB
+# try:
+#     import matlab.engine
+#     ml_eng_installed = True
+# except ImportError:
+#     print("Unable to detect MATLAB engine.")
+#     ml_eng_installed = False
 
-if ml_eng_installed:
-    all_ml = matlab.engine.find_matlab()
-    if len(all_ml) == 0:
-        print("No shared MATLAB engine sessions found. Type matlab.engine.shareEngine at the MATLAB command prompt")
-    elif len(all_ml) == 1:
-        future = matlab.engine.connect_matlab(all_ml[0], background=True)
-        eng = future.result()
-        mw = eng.workspace
-    elif len(all_ml) > 1:
-        print("Multiple MATLAB sessions found. Please connect manually.")
+# if ml_eng_installed:
+#     all_ml = matlab.engine.find_matlab()
+#     if len(all_ml) == 0:
+#         print("No shared MATLAB engine sessions found. Type matlab.engine.shareEngine at the MATLAB command prompt")
+#     elif len(all_ml) == 1:
+#         future = matlab.engine.connect_matlab(all_ml[0], background=True)
+#         eng = future.result()
+#         mw = eng.workspace
+#     elif len(all_ml) > 1:
+#         print("Multiple MATLAB sessions found. Please connect manually.")
